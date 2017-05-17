ac128e6575198d9142916c8539c90afa4d878709
Jordi <jfarre@synergie.es>
2017-05-17 11:59:43 +0200

    Solo commiteo el archivo ActualizarRepositorio.rb, script para hacer los commit mas rapido.

---
+++ ActualizarRepositorio.rb
@@ -0,0 +1,8 @@
+cd C:\RAILS\Ejercicio_URL_Shorter
+git status
+git add .
+git status
+git commit -m 'Sigo intentando que cargue aunque no funcione el formulario de registro de URLs'
+git status
+git push -u origin master
+git status
