a30da270b31ce83682ae8691539dff59d775a66f
Jordi <jfarre@synergie.es>
2017-05-17 11:36:44 +0200

    Sigo intentando que cargue aunque no funcione el formulario de registro de URLs

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,5 +1,20 @@
 class ShortUrlsController < ApplicationController
   def new
-      @short_urls = Short_url.new
+      @short_urls = ShortUrl.new(short_url_params)
   end
+
+  def create
+      @short_urls = ShortUrl.new(short_url_params)
+  end
+
+  private
+
+	def short_url_params
+		params.require(:short_url).permit(:url)
+
+    # hacemos q el campo url
+		# sea permitido pero implicitamente estamos HACIENDO INACCESIBLES
+		# desde nuestra aplicacion Web LOS OTROS campos como url_id PARA EVITAR
+		# ATAQUES DE USUARIOS MALICIOSOS
+	end
 end
