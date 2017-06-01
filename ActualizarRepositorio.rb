e7dffa767880dcc548757e88938e047fa84a0690
Jordi <jfarre@synergie.es>
2017-06-01 18:48:03 +0200

    parte 2 (me he cargado algo del /short_urls/:id), parte 3(he creado Admin sin botones DELETE)

--- ActualizarRepositorio.rb
+++ ActualizarRepositorio.rb
@@ -2,7 +2,7 @@ cd C:\RAILS\Ejercicio_URL_Shorter
 git status
 git add .
 git status
-git commit -m "Sigo intentando que cargue aunque no funcione el formulario de registro de URLs"
+git commit -m ""
 git status
 git push -u origin master
 git status
